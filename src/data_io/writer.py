810401f5f28af550eacec511808d7c8d86710c8f
csorgod <csorgodev@hotmail.com>
2026-06-21 20:42:51 -0300

    Resolvido bug de execução no docker por conta do sistema de arquivos bloqueado para exclusão

--- src/data_io/writer.py
+++ src/data_io/writer.py
@@ -1,17 +1,19 @@
+import shutil
+import os
+
 from pyspark.sql import DataFrame
 
 from config.settings import Settings
 
 
 class ParquetWriter:
-    """ Classe responsável pela leitura de arquivos parquet """ 
+    """ Classe responsável pela escrita de arquivos parquet """
 
     def __init__(self, settings: Settings):
         self._settings = settings
 
     def write(self, df: DataFrame) -> None:
-        (
-            df.write
-            .mode("overwrite")
-            .parquet(self._settings.output_path)
-        )
+        if os.path.exists(self._settings.output_path):
+            shutil.rmtree(self._settings.output_path)
+
+        df.write.parquet(self._settings.output_path)
